7e78a3618421ae8e702ea7d57a48712372b87d63
Minki Kim <alsrl0023@naver.com>
2025-05-26 00:01:37 +0900

    fix: RefreshToken CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(8080);
