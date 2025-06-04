c20c0f9a803c4208bad68f4f47017cdd8e1dfdaa
minki <alsrl0023@naver.com>
2025-06-04 11:39:12 +0900

    fix: main.ts cors:true 제거

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:3000ß'),
+    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:3000'),
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
